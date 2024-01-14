1bef2d8b8d5aae76b2efa85dafd18b8c919b15c4
AnjasFedo <fedoaf98@gmail.com>
2024-01-14 22:28:02 +0700

    Create loadEnv Function to Load some Variable on .env

--- main.go
+++ main.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"github.com/slack-go/slack"
+	"log"
 	"os"
+	"strings"
+
+	"github.com/slack-go/slack"
 )
 
 func main() {
-	
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+	channerArr := []string{os.Getenv("CHANNEL_ID")}
+
+	fileArr := []string{""}
+}
+
+func loadEnv() error {
+	file, err := os.Open(".env")
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		pair := strings.SplitN(line, "=", 2)
+
+		if len(pair) == 2 {
+			key, value := pair[0], pair[2]
+
+			os.Setenv(key, value)
+		}
+
+		if err := scanner.Err(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
