d30c82a1beb3cc2b08afb7c31bc63f7fb13cab5a
AnjasFedo <fedoaf98@gmail.com>
2024-01-14 22:53:36 +0700

    Bot Uploaded File to Slack Channel with

--- main.go
+++ main.go
@@ -11,11 +11,29 @@ import (
 )
 
 func main() {
+	if err := loadEnv(); err != nil {
+		log.Fatal("Error on Load .env:", err)
+	}
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 
 	channerArr := []string{os.Getenv("CHANNEL_ID")}
 
-	fileArr := []string{""}
+	fileArr := []string{"./file/img-blank.PNG"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channerArr,
+			File:     fileArr[i],
+		}
+
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			return
+		}
+
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
 }
 
 func loadEnv() error {
@@ -27,20 +45,19 @@ func loadEnv() error {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
 	for scanner.Scan() {
-		line := scanner.Text()
+		line := strings.TrimSpace(scanner.Text())
 
-		pair := strings.SplitN(line, "=", 2)
-
-		if len(pair) == 2 {
-			key, value := pair[0], pair[2]
-
-			os.Setenv(key, value)
+		if line == "" {
+			continue
 		}
 
-		if err := scanner.Err(); err != nil {
-			return err
+		pair := strings.SplitN(line, "=", 2)
+		if len(pair) == 2 {
+			key, value := pair[0], pair[1]
+			os.Setenv(key, value)
+		} else {
+			log.Printf("Invalid line in .env file: %s", line)
 		}
 	}
 
